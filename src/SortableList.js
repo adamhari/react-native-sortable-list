a92634be3af6aeb8bc5dfbe36212a99fe235784d
Adam-Hari <adam.hari@coxautoinc.com>
2019-10-09 12:51:58 -0600

    tweak findRowUnderActiveRow's logic

--- src/SortableList.js
+++ src/SortableList.js
@@ -452,7 +452,7 @@ export default class SortableList extends Component {
 
       	if (
 					(rowTopY <= y  && rowBottomY >= y) ||
-					(activeRowIndex === 0 && rowBottomY >= y)
+					(activeRowKey === 0 && rowBottomY >= y)
 				) {
 					return {
 						rowKey: order[currentRowIndex],
